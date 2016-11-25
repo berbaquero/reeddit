fae784757232393b6e1677a7632fcc2cc3d86f5c
Bernardo Baquero Stand <baquerober@gmail.com>
2016-11-25 23:19:22 +0100

    Refactor context/feature detection module

--- scripts/modules/_is.js
+++ scripts/modules/_is.js
@@ -1,46 +1,31 @@
-var wideScreenBP = window.matchMedia("(min-width: 1000px)"),
-	largeScreenBP = window.matchMedia("(min-width: 490px)"),
-	UA = window.navigator.userAgent;
+const wideScreenBP = window.matchMedia("(min-width: 1000px)");
+const largeScreenBP = window.matchMedia("(min-width: 490px)");
+const UA = window.navigator.userAgent;
 
-var is = (function() {
+const is = (function() {
 
-	// Do detection
-	var isDesktop = !/iPhone|iPod|iPad|Android|Mobile/.test(UA),
-		isiPad = /iPad/.test(UA),
-		isiPhone = /iP(hone|od)/.test(UA),
-		isiOS = (isiPhone || isiPad);
+  // Do detection
+  var isDesktop = !/iPhone|iPod|iPad|Android|Mobile/.test(UA);
+  var isiPad = /iPad/.test(UA);
+  var isiPhone = /iP(hone|od)/.test(UA);
+  var isiOS = (isiPhone || isiPad);
 
-	var isLinkDownloadable = (() => {
-		var link = document.createElement('a');
-		return link.download !== undefined;
-	})();
+  const iOSversion = (() => {
+    if (!isiOS) {
+      return 0;
+    }
+    return parseInt(UA.match(/ OS (\d+)_/i)[1], 10);
+  })();
 
-	const iOSversion = (() => {
-			if (!isiOS) {
-				return 0;
-			}
-			return parseInt(UA.match(/ OS (\d+)_/i)[1], 10);
-		})();
-
-	return {
-
-		wideScreen: wideScreenBP.matches,
-
-		largeScreen: largeScreenBP.matches,
-
-		desktop: isDesktop,
-
-		mobile: !isDesktop,
-
-		iPhone: isiPhone,
-
-		iPad: isiPad,
-
-		iOS: isiOS,
-
-		iOS7: (isiOS && iOSversion >= 7),
-
-		linkDownloadable: isLinkDownloadable
-	};
+  return {
+    wideScreen: wideScreenBP.matches,
+    largeScreen: largeScreenBP.matches,
+    desktop: isDesktop,
+    mobile: !isDesktop,
+    iPhone: isiPhone,
+    iPad: isiPad,
+    iOS: isiOS,
+    iOS7: (isiOS && iOSversion >= 7),
+  };
 
 })();
